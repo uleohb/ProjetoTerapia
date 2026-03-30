8c44e25d53735c20a2d771d7365bcf7ca6bd5001
Leonardo <hbleon07@gmail.com>
2026-03-30 15:45:03 -0300

    Separando versão antiga

---
+++ ProjetoTerapia/Pages/Teste.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoTerapia.Pages
+{
+    public class TesteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
